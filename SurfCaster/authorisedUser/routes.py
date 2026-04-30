d5713733e1cf3c82206498a77977599819d0c84c
wrenstuff <stewart.nzwren@gmail.com>
2026-04-30 15:32:31 +1200

    Added authorised user routes
    Authorised user routes added and imported into app.py

--- SurfCaster/authorisedUser/routes.py
+++ SurfCaster/authorisedUser/routes.py
@@ -1,1 +1,38 @@
 # authorisedUser/routes.py
+
+# library imports
+from flask import Blueprint
+
+# blueprint creation
+user_routes = Blueprint('user_routes', __name__)
+
+# route definitions
+# Dashboard
+@user_routes.route('/')
+def home():
+    return "Authorised User Dashboard"
+
+# URL Scanner
+@user_routes.route('/scan')
+def scan():
+    return "Authorised User URL Scan"
+
+# Scan History
+@user_routes.route('/history')
+def history():
+    return "Authorised User Scan History"
+
+# Account
+@user_routes.route('/account') # possibly change to username?
+def account():
+    return "Authorised User Account"
+
+# Settings
+@user_routes.route('/settings')
+def settings():
+    return "Authorised User Settings"
+
+# Support
+@user_routes.route('/support')
+def support():
+    return "Authorised User Support"
