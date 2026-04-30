cf5f4cd7b9b8b3d80a577e037bdac1b47b466c8a
wrenstuff <stewart.nzwren@gmail.com>
2026-04-30 13:40:13 +1200

    Unauthorised User routes added
    Added:
    - Unauthorised user routing.
    Fixed:
    - app.py has correct imports now

--- SurfCaster/app.py
+++ SurfCaster/app.py
@@ -4,17 +4,19 @@
 from flask import Flask
 
 # internal imports
-from admin import routes as admin_routes
-from reviewer import routes as reviewer_routes
-from authorisedUser import routes as user_routes
-from unauthorisedUser import routes as unauthorised_user_routes
+# Uncomment when routes have been added
+#from admin.routes import admin_routes
+#from reviewer.routes import reviewer_routes
+#from authorisedUser.routes import user_routes
+from unauthorisedUser.routes import unauthorised_user_routes
 
 # app creation and blueprint registration
 app = Flask(__name__)
-app.register_blueprint(admin_routes)
-app.register_blueprint(reviewer_routes)
-app.register_blueprint(user_routes)
-app.register_blueprint(unauthorised_user_routes)
+
+#app.register_blueprint(admin_routes, url_prefix='/admin')
+#app.register_blueprint(reviewer_routes, url_prefix='/reviewer')
+#app.register_blueprint(user_routes, url_prefix='/user')
+app.register_blueprint(unauthorised_user_routes, url_prefix='/')
 
 # run the app
 if __name__ == "__main__":
--- SurfCaster/unauthorisedUser/routes.py
+++ SurfCaster/unauthorisedUser/routes.py
@@ -1,1 +1,28 @@
 # unauthorisedUser/routes.py
+
+# library imports
+from flask import Blueprint
+
+# blueprint creation
+unauthorised_user_routes = Blueprint('unauthorised_user_routes', __name__)
+
+# route definitions
+# Dashboard
+@unauthorised_user_routes.route('/')
+def home():
+    return "Unauthorised User Dashboard"
+
+# URL Scanner
+@unauthorised_user_routes.route('/scan')
+def scan():
+    return "Unauthorised User URL Scan"
+
+# Settings
+@unauthorised_user_routes.route('/settings')
+def settings():
+    return "Unauthorised User Settings"
+
+# Support
+@unauthorised_user_routes.route('/support')
+def support():
+    return "Unauthorised User Support"
