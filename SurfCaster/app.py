f55e29d5e237768407cef8377f67e8aef8371cb0
wrenstuff <stewart.nzwren@gmail.com>
2026-04-30 15:47:03 +1200

    reviewer routes
    Added reviewer routing
    changed anauthorised user prefix to "unauth"

--- SurfCaster/app.py
+++ SurfCaster/app.py
@@ -6,7 +6,7 @@ from flask import Flask
 # internal imports
 # Uncomment when routes have been added
 #from admin.routes import admin_routes
-#from reviewer.routes import reviewer_routes
+from reviewer.routes import reviewer_routes
 from authorisedUser.routes import user_routes
 from unauthorisedUser.routes import unauthorised_user_routes
 
@@ -14,9 +14,9 @@ from unauthorisedUser.routes import unauthorised_user_routes
 app = Flask(__name__)
 
 #app.register_blueprint(admin_routes, url_prefix='/admin')
-#app.register_blueprint(reviewer_routes, url_prefix='/reviewer')
+app.register_blueprint(reviewer_routes, url_prefix='/reviewer')
 app.register_blueprint(user_routes, url_prefix='/user')
-app.register_blueprint(unauthorised_user_routes, url_prefix='/')
+app.register_blueprint(unauthorised_user_routes, url_prefix='/unauth')
 
 # run the app
 if __name__ == "__main__":
--- SurfCaster/reviewer/routes.py
+++ SurfCaster/reviewer/routes.py
@@ -1,1 +1,48 @@
 # reviewer/routes.py
+
+# library imports
+from flask import Blueprint
+
+# blueprint creation
+reviewer_routes = Blueprint('reviewer_routes', __name__)
+
+# route definitions
+# Dashboard
+@reviewer_routes.route('/')
+def home():
+    return "Reviewer Dashboard"
+
+# URL Scanner
+@reviewer_routes.route('/scan')
+def scan():
+    return "Reviewer URL Scan"
+
+# Scan History
+@reviewer_routes.route('/history')
+def history():
+    return "Reviewer Scan History"
+
+# Account
+@reviewer_routes.route('/account') # possibly change to username?
+def account():
+    return "Reviewer Account"
+
+# Settings
+@reviewer_routes.route('/settings')
+def settings():
+    return "Reviewer Settings"
+
+# Support
+@reviewer_routes.route('/support')
+def support():
+    return "Reviewer Support"
+
+# Review Queue
+@reviewer_routes.route('/queue')
+def queue():
+    return "Reviewer Review Queue"
+
+# Review History
+@reviewer_routes.route('/reviews')
+def reviews():
+    return "Reviewer Review History"
