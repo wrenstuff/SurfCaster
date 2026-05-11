e7530ead7cad38c0700e612488acfeeed5b43608
Cody Johns <nzchronos@yahoo.co.nz>
2026-05-11 16:59:16 +1200

    Login-page
    Fixed routing and done basic css

--- SurfCaster/auth/routes.py
+++ SurfCaster/auth/routes.py
@@ -17,18 +17,6 @@ def login():
     #print("Login failed") # Debug statement
     #return "Login Page" */
 
-# Recovery Page
-@auth_routes.route('/forgotpassword')
-def recovery():
-    return render_template('forgotpassword.html')
-    #print("Recovery attempt") # Debug statement
-    #if True: # Placeholder for actual recovery logic
-        #print("Recovery successful") # Debug statement
-        #return "Recovery Page"
-    #else:
-        #return "Recovery Failed"                
-    
-
 # Registration Page
 @auth_routes.route('/recover')
 def recover():
@@ -40,3 +28,14 @@ def recover():
     #else:   
         #print("Registration failed") # Debug statement
         #return "Registration Page"   
+
+@auth_routes.route('/signup')
+def signup():
+    return render_template('signup.html')
+    #print("Registration attempt") # Debug statement
+    #if True: # Placeholder for actual registration logic
+        #print("Registration successful") # Debug statement
+        #return "Login Page"
+    #else:   
+        #print("Registration failed") # Debug statement
+        #return "Registration Page"  
