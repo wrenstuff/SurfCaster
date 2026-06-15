4cf95f45de3a2dd3ce26bc3acf76c9cf5e8b123d
Wren <stewart.nzwren@gmail.com>
2026-06-15 19:06:23 +1200

    looking better :D

--- SurfCaster/extensions.py
+++ SurfCaster/extensions.py
@@ -6,6 +6,7 @@ from sqlalchemy import text
 from datetime import datetime
 import time
 import calendar
+from colour_helper import get_gradient
 
 db = SQLAlchemy()
 
@@ -77,6 +78,11 @@ def get_scan_history():
         return []
     history_path = 'scan_history.json'
     history = load_json_file(history_path, [])
+
+    for scan in history:
+        score = scan.get('last_scan_result', 0)
+        scan['color'] = get_gradient(score)
+
     return history
 
 def get_reviews():
